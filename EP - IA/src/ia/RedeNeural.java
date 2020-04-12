3596a7e165983149dff8ce913835732345710dd1
JonatasDantas <jonatas.almeida14@hotmail.com>
2020-04-12 19:03:51 -0300

    Dinamização da Rede Neural e utilizando bias nos calculos

--- EP - IA/src/ia/RedeNeural.java
+++ EP - IA/src/ia/RedeNeural.java
@@ -10,7 +10,8 @@ public class RedeNeural {
 	private static double taxaAprendizado;
 	public static int inputNeuronio;
 	public static int hiddenNeuronio;
-	private static double[] bias = new double[2];
+	private static double[] biasHidden;
+	private static double[] biasOutput;
 	private static Neuronio[] neuronios;
 	public int epocas;
 	public static double[] saidasEsperadas;
@@ -19,9 +20,7 @@ public class RedeNeural {
 
 	public void inicializaVariaveis() throws IOException {
 		Leitor leitor = new Leitor();
-		
-		bias[0] = getRandomNoIntervalo(0.0, 1.0);
-		bias[1] = getRandomNoIntervalo(0.0, 1.0);
+
 		taxaAprendizado = getRandomNoIntervalo(0.1, 1.0);
 		epocas = 1000;
 		saidasEsperadas = leitor.leSaidaEsperada();
@@ -30,6 +29,9 @@ public class RedeNeural {
 	
 	public void inicializaNeuronios(int qtdInput, int qtdHidden, int qtdOutuput) {
 		neuronios = new Neuronio[qtdInput + qtdHidden + qtdOutuput];
+		biasHidden = new double[qtdHidden];
+		biasOutput = new double[qtdOutuput];
+
 		inputNeuronio = qtdInput;
 		hiddenNeuronio = qtdHidden;
 		
@@ -37,14 +39,21 @@ public class RedeNeural {
 			if (i < qtdInput) {
 				neuronios[i] = new Neuronio(TipoNeuronio.I,0);
 			} else if (i < (qtdInput + qtdHidden)) {
-				neuronios[i] = new Neuronio(TipoNeuronio.H, qtdHidden);
+				neuronios[i] = new Neuronio(TipoNeuronio.H, qtdInput);
 			} else {
 				neuronios[i] = new Neuronio(TipoNeuronio.O, qtdHidden);
 			}
 		}
-		
+
+		for (int i = 0; i < biasHidden.length; i++) {
+			biasHidden[i] = getRandomNoIntervalo(0.0, 1.0);
+		}
+
+		for (int i = 0; i < biasOutput.length; i++) {
+			biasOutput[i] = getRandomNoIntervalo(0.0, 1.0);
+		}
 	}
-	
+
 	public Neuronio[] getNeuronios() {
 		return neuronios;
 	}
@@ -61,25 +70,28 @@ public class RedeNeural {
 	
 	public RedeNeural forwardprop (double input[]) {
 		double somaPesos = 0;
-		//double somaEntrada = 0;
-		
 		
 		for (int i=0; i<neuronios.length; i++) {
-			
 			switch(neuronios[i].getTiponeuronio()) {
 				case I:
 					neuronios[i].setOutput(input[i]);
 					break;
 				case H:
-					somaPesos = neuronios[i].getEntrada() +
-								neuronios[i].getPeso()[0] * neuronios[0].getOutput() +
-								neuronios[i].getPeso()[1] * neuronios[1].getOutput();
+					somaPesos = biasHidden[i - inputNeuronio];
+
+					for (int j = 0; j < inputNeuronio; j++) {
+						somaPesos += neuronios[i].getPeso()[j] * neuronios[j].getOutput();
+					}
+
 					neuronios[i].aplicaFuncaoAtivacao(somaPesos);
 					break;
 				case O:
-					somaPesos = neuronios[i].getEntrada() +
-								neuronios[i].getPeso()[0] * neuronios[2].getOutput() +
-								neuronios[i].getPeso()[1] * neuronios[3].getOutput();
+					somaPesos = biasOutput[i - (inputNeuronio + hiddenNeuronio)];
+
+					for (int j = inputNeuronio; j < (inputNeuronio + hiddenNeuronio); j++) {
+						somaPesos += neuronios[i].getPeso()[j - inputNeuronio] * neuronios[j].getOutput();
+					}
+
 					neuronios[i].aplicaFuncaoAtivacao(somaPesos);
 					break;
 			}
@@ -89,20 +101,36 @@ public class RedeNeural {
 	}
 	
 	public RedeNeural backpropError (double valEsperado) {
-		neuronios[4].setError((valEsperado - neuronios[4].getOutput()) * neuronios[4].calculaDerivada());
-		neuronios[4].setEntrada(neuronios[4].getEntrada() + taxaAprendizado * neuronios[4].getError());
-		neuronios[4].getPeso()[0] = neuronios[4].getPeso()[0] + taxaAprendizado * neuronios[4].getError() * neuronios[2].getOutput();
-		neuronios[4].getPeso()[1] = neuronios[4].getPeso()[1] + taxaAprendizado * neuronios[4].getError() * neuronios[3].getOutput();
-		
-		neuronios[3].setError(neuronios[4].getPeso()[1] * neuronios[4].getError() * neuronios[3].calculaDerivada());
-		neuronios[3].setEntrada(neuronios[3].getEntrada() + taxaAprendizado * neuronios[3].getError());
-		neuronios[3].getPeso()[0] = neuronios[3].getPeso()[0] + taxaAprendizado * neuronios[3].getError() * neuronios[0].getOutput();
-		neuronios[3].getPeso()[1] = neuronios[3].getPeso()[1] + taxaAprendizado * neuronios[3].getError() * neuronios[1].getOutput();
-		
-		neuronios[2].setError(neuronios[4].getPeso()[0] * neuronios[4].getError() * neuronios[2].calculaDerivada());
-		neuronios[2].setEntrada(neuronios[2].getEntrada() + taxaAprendizado * neuronios[2].getError());
-		neuronios[2].getPeso()[0] = neuronios[2].getPeso()[0] + taxaAprendizado * neuronios[2].getError() * neuronios[0].getOutput();
-		neuronios[2].getPeso()[1] = neuronios[2].getPeso()[1] + taxaAprendizado * neuronios[2].getError() * neuronios[1].getOutput();
+		// Passo 6 - backpropagation camada de saída.
+		for (int i = neuronios.length -1; i >= inputNeuronio + hiddenNeuronio; i--) {
+			neuronios[i].setError((valEsperado - neuronios[i].getOutput()) * neuronios[i].calculaDerivada());
+			neuronios[i].setEntrada(neuronios[i].getEntrada() + taxaAprendizado * neuronios[i].getError());
+
+			for (int j = 0; j < hiddenNeuronio; j++) {
+				neuronios[i].getPeso()[j] = neuronios[i].getPeso()[j] + taxaAprendizado * neuronios[i].getError() * neuronios[j + hiddenNeuronio].getOutput();
+			}
+
+			biasOutput[i - (inputNeuronio + hiddenNeuronio)] = biasOutput[i - (inputNeuronio + hiddenNeuronio)] + (taxaAprendizado * neuronios[i].getError());
+		}
+
+		// Passo 7 - backpropagation camada escondida.
+		for (int i = inputNeuronio; i < inputNeuronio + hiddenNeuronio; i++) {
+			double somaErro = 0;
+
+			for (int j = neuronios.length -1; j >= inputNeuronio + hiddenNeuronio; j--) {
+				somaErro += neuronios[j].getPeso()[i - inputNeuronio] * neuronios[j].getError();
+			}
+
+			neuronios[i].setError(somaErro * neuronios[i].calculaDerivada());
+			neuronios[i].setEntrada(neuronios[i].getEntrada() + taxaAprendizado * neuronios[i].getError());
+
+			for (int j = 0; j < inputNeuronio; j++) {
+				neuronios[i].getPeso()[j] = neuronios[i].getPeso()[j] + taxaAprendizado * neuronios[i].getError() * neuronios[j].getOutput();
+			}
+
+			biasHidden[i - inputNeuronio] = biasHidden[i - inputNeuronio] + (taxaAprendizado * neuronios[i].getError());
+		}
+
 		return this;
 	}
 }
